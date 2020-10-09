f8a14923807929e671e4dd71494a63989af915c0
soualexandre <alexandreifto2@gmail.com>
2020-10-09 19:26:30 -0300

    Primeiro

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import GoogleLogin from 'react-google-login';
 
 function App() {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [profilePic, setProfilePic] = useState();
+  const [isLoguedIn, setIsLoguedIn] = useState(false);
+
+
+  const responseGoogle = (response) => {
+    console.log(response);
+    const {
+      profileObj: { name, email, imageUrl },
+    } = response;
+    setName(name);
+    setEmail(email);
+    setProfilePic(imageUrl);
+    setIsLoguedIn(true)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="container">
+      <GoogleLogin
+        clientId="573865740494-v8jjhutdt9s76lq9qmgjtiel89pocoh4.apps.googleusercontent.com"
+        buttonText="container com o login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+
+      />
+      {isLoguedIn ? (
+        <div>
+          <h1>User Information</h1>
+          <img src={profilePic} alt="Profile" />
+          <p>name: {name}</p>
+          <p>Email: {email}</p>
+          </div>
+      ): (
+        ""
+      )}
+
+        </div>
+      );
 }
 
 export default App;
